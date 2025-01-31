683a8a9e245576bb3cdd962eb91c4f4b09525927
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-01-31 19:44:35 +0530

    Initial Commit

---
+++ src/main/java/com/StarkIndustries/NotesAppBackend/NotesAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.StarkIndustries.NotesAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StarkIndustries/NotesAppBackend/NotesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarkIndustries.NotesAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
